b4d3bd2cc34fb0a824fff14fdfa9c785d0161243
Hamish Coleman <hamish@zot.org>
2010-09-26 13:06:48 +1000

    Add missing definition

--- scm.h
+++ scm.h
@@ -8,13 +8,14 @@ struct SCM_def {
 	char *name;
 	char *desc;
 	int mode;			/* set to SVC_CONSOLE by the *-scm.c code */
+	int argc; char **argv;		/* original cmdline args */
 	int (*init)(int, char **);	/* called before main */
 	int (*main)(int);		/* called to run the service */
 	int (*stop)(int);		/* called by scm to tell the service to stop */
 };
 
 int SCM_Start(struct SCM_def *, int argc, char **argv);
-char *SCM_Install(struct SCM_def *);
+char *SCM_Install(struct SCM_def *,char *);
 int SCM_Remove(struct SCM_def *);
 
 #define SVC_OK		0
